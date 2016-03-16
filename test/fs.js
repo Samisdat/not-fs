4c8f1526e4c22032b63cab546f1966a004429199
samisdat <bastian.pertz@gmail.com>
2016-03-16 18:56:53 +0000

    test fileExistSync

--- test/fs.js
+++ test/fs.js
@@ -3,17 +3,71 @@
 var expect = require('chai').expect;
 
 var fs = require('fs');
+var path = require("path");
 
-describe('method qfs.fileExists', function() {
+/**
+ * https://gist.github.com/tkihira/2367067
+ */
+var rmdir = function(dir) {
+    var list = fs.readdirSync(dir);
+    for(var i = 0; i < list.length; i++) {
+        var filename = path.join(dir, list[i]);
+        var stat = fs.statSync(filename);
+        
+        if(filename == "." || filename == "..") {
+            // pass these files
+        } else if(stat.isDirectory()) {
+            // rmdir recursively
+            rmdir(filename);
+        } else {
+            // rm fiilename
+            fs.unlinkSync(filename);
+        }
+    }
+    fs.rmdirSync(dir);
+};
+
+describe('method fs.existsSync', function() {
 
     beforeEach(function() {
+        if(true === fs.existsSync('/tmp/vfs-test')){
+            rmdir('/tmp/vfs-test');
+        }
+
+        fs.mkdirSync('/tmp/vfs-test');
+        fs.mkdirSync('/tmp/vfs-test/exist');
+        fs.writeFileSync('/tmp/vfs-test/message.txt', 'Hello Node.js', 'utf8');
     });
 
-    it('file exist tmp', function() {
+    it('succeed on existing dir', function() {
 
-        var promise = fs.existsSync('/tmp');
+        var exist = fs.existsSync('/tmp/vfs-test/exist');
 
-        console.log(promise);
+        expect(exist).to.be.true;
+
+    });
+
+    it('fail on not existing dir', function() {
+
+        var exist = fs.existsSync('/tmp/vfs-test/not-exist');
+
+        expect(exist).to.be.false;
+
+    });
+
+    it('succeed on existing file', function() {
+
+        var exist = fs.existsSync('/tmp/vfs-test/message.txt');
+
+        expect(exist).to.be.true;
+
+    });
+
+    it('fail on not existing file', function() {
+
+        var exist = fs.existsSync('/tmp/vfs-test/no-message.txt');
+
+        expect(exist).to.be.false;
 
     });
 
