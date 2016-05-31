33acfe7444f7c79119858400a965ae99f7eade72
samisdat <bastian.sackermann@gmail.com>
2016-05-31 22:43:10 +0000

    add dir and file and tests

--- test/unreal/tree.js
+++ test/unreal/tree.js
@@ -13,27 +13,66 @@ describe('tree', function() {
 
         var tree = new Tree();
 
-        expect(tree).to.be.instanceof(Tree);        
-
-        tree.addDir('/foo/');
-        tree.addDir('./foo');
-        tree.addDir('/foo');
-        tree.addDir('/foo/two/three/four');
-        tree.addDir('/foo/two/a');
-        tree.addDir('/foo/two/b');
-        console.log(tree.exists('/foo/two/three/four'));
-
-        tree.log();
-        //tree.addDir('/foo/bar/');
-        //tree.addDir('/foo/bar/foobar/');
+        expect(tree).to.be.instanceof(Tree);
 
     });
 
-    it.skip('has incrementing inodeNumber', function() {
+    it('resolves dirs correct', function() {
+
+        var tree = new Tree();
+
+        expect(tree.resolveDir('/')).to.be.equal('/');
+        expect(tree.resolveDir('/foo')).to.be.equal('/foo');
+        expect(tree.resolveDir('/foo/')).to.be.equal('/foo');
+        expect(tree.resolveDir('/foo/bar')).to.be.equal('/foo/bar');
+        expect(tree.resolveDir('/foo/bar/')).to.be.equal('/foo/bar');
+
+        //@TODO relative path
+        expect(function(){
+            tree.resolveDir('./relative')
+        }).to.throw('Only absolute path supported');
+
+    });
+
+    it('has incrementing inodeNumber', function() {
 
         var tree = new Tree();
 
         expect(tree.getLastInodeNumber()).to.be.equal(0);
+
+        tree.addDir('/foo', true);
+
+        expect(tree.getLastInodeNumber()).to.be.equal(1);
+        
+        tree.addDir('/foo/bar', true);
+
+        expect(tree.getLastInodeNumber()).to.be.equal(2);
+
+    });
+
+    it('can add dir', function() {
+
+        var tree = new Tree();
+
+        expect(tree.exists('/foo')).to.be.false;
+        expect(tree.exists('/foo/bar')).to.be.false;
+
+        tree.addDir('/foo/bar', true);
+        
+        expect(tree.exists('/foo')).to.be.true;
+        expect(tree.exists('/foo/bar')).to.be.true;
+
+    });
+
+    it('can add file', function() {
+
+        var tree = new Tree();
+
+        expect(tree.exists('/foo/bar.txt')).to.be.false;
+
+        tree.addFile('/foo/bar.txt', 'foobar');
+
+        expect(tree.exists('/foo/bar.txt')).to.be.true;
 
     });
 
