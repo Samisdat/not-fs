607c56e2b7deda0f066d531b1eb32f590953cd34
samisdat <bastian.sackermann@gmail.com>
2016-06-09 20:36:23 +0000

    add test for removing a dir and keep it's siblings untouched

--- test/lib/tree.js
+++ test/lib/tree.js
@@ -115,6 +115,41 @@ describe('tree', function() {
 
     });
 
+    it('can remove dir without its siblings', function() {
+
+        var tree = new Tree();
+
+        expect(tree.tree).to.be.deep.equal({0: []});
+        expect(tree.parent).to.be.deep.equal({});
+        //@TODO test for leafs
+
+        tree.addDir('/tmp/unreal-fs', true);
+        tree.addDir('/tmp/unreal-fs/exist');
+        tree.addFile('/tmp/unreal-fs/message.txt', 'Hello Node.js');
+
+        expect(tree.exists('/tmp')).to.be.true;
+        expect(tree.exists('/tmp/unreal-fs')).to.be.true;
+        expect(tree.exists('/tmp/unreal-fs/exist')).to.be.true;
+        expect(tree.exists('/tmp/unreal-fs/message.txt')).to.be.true;
+
+//        expect(tree.tree).to.be.deep.equal( {0: [ 1 ], 1: [ 2, 3, 5 ], 2: [], 3: [ 4, 6 ], 4: [] });
+//        expect(tree.parent).to.be.deep.equal( { 1: 0, 2: 1, 3: 1, 4: 3, 5: 1, 6: 3 });
+
+        tree.removeDir('//tmp/unreal-fs/exist');
+
+        expect(tree.exists('/tmp')).to.be.true;
+        expect(tree.exists('/tmp/unreal-fs')).to.be.true;
+        expect(tree.exists('/tmp/unreal-fs/exist')).to.be.false;
+        expect(tree.exists('/tmp/unreal-fs/message.txt')).to.be.true;
+
+        //expect(tree.tree).to.be.deep.equal({0: []});
+        //expect(tree.parent).to.be.deep.equal({});
+
+
+    });
+
+
+
     it('is dir', function() {
 
         var tree = new Tree();
