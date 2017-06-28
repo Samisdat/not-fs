76d4be75e95e93f9c15ae4a80f64e4398a8ff5c9
Bastian Pertz <bastian.pertz@vodafone.com>
2017-06-28 21:27:37 +0200

    Migrate node

---
+++ typescript/src/node.test.ts
@@ -0,0 +1,96 @@
+'use strict';
+
+import { expect } from 'chai';
+
+import Node from './node';
+
+describe('node', function() {
+
+    let node:Node;
+
+    beforeEach(function() {
+    });
+
+    it('can be created', function() {
+
+        node = new Node(1, 'test');
+
+        expect(node).to.be.instanceof(Node);
+
+    });
+
+    it('get it\'s inodenumber', function() {
+
+        expect(node.getInodeNumber()).to.be.equal(1);
+
+    });
+
+    it('get it\'s name', function() {
+
+        expect(node.getName()).to.be.equal('test');
+
+        node.setName('foobar');
+        expect(node.getName()).to.be.equal('foobar');
+
+    });    
+
+    it('get/set permission', function() {
+
+        expect(node.getPermission().getMode()).to.be.equal('0755');
+
+        node.setPermission('0777');
+        expect(node.getPermission().getMode()).to.be.equal('0777');
+
+    });
+
+
+    it('isRoot is false', function() {
+
+        expect(node.isRoot()).to.be.false;
+
+    });
+
+    it('isFile is undefined', function() {
+
+        expect(node.isFile()).to.be.undefined;
+
+    });
+
+    it('isDirectory is undefined', function() {
+
+        expect(node.isDirectory()).to.be.undefined;
+
+    });
+    
+    it('isBlockDevice is false', function() {
+
+        expect(node.isBlockDevice()).to.be.false;
+
+    });
+
+    it('isCharacterDevice is false', function() {
+
+        expect(node.isCharacterDevice()).to.be.false;
+
+    });
+
+    it('isSymbolicLink is false', function() {
+
+        expect(node.isSymbolicLink()).to.be.false;
+
+    });
+
+    it('isFIFO is false', function() {
+
+        expect(node.isFIFO()).to.be.false;
+
+    });
+
+    it('isSocket is false', function() {
+
+        expect(node.isSocket()).to.be.false;
+
+    });
+
+});
+
---
+++ typescript/src/node.ts
@@ -0,0 +1,98 @@
+'use strict';
+
+import Permission from './permission';
+
+export default class Node {
+
+    private inodeNumber:number;
+    private name:string;
+
+    protected permission:Permission
+
+    constructor(inodeNumber:number, name:string) {
+        
+        this.inodeNumber = inodeNumber;
+        this.name = name;
+
+        this.permission = new Permission('0755');
+
+    }
+
+    public getInodeNumber():number{
+
+        return this.inodeNumber;
+    }
+
+    public getName():string{
+
+        return this.name;
+    }
+
+    public setName(name:string):void{
+
+        this.name = name;
+    }
+
+    public getPermission():Permission{
+
+        return this.permission;
+
+    };
+
+    public setPermission = function(permission:string){
+
+        this.permission.setMode(permission);
+
+    };
+
+    public isRoot():boolean{
+
+        return false;
+
+    };
+
+    public isFile():boolean{
+
+        return undefined;
+
+    };
+
+    public isDirectory():boolean{
+
+        return undefined;
+
+    };
+
+    public isBlockDevice():boolean{
+
+        return false;
+
+    };
+
+    public isCharacterDevice():boolean{
+
+        return false;
+
+    };
+
+    public isSymbolicLink():boolean{
+
+        return false;
+
+    };
+
+    public isFIFO():boolean{
+
+        return false;
+
+    };
+
+    public isSocket():boolean{
+
+        return false;
+
+    };
+
+
+}
+
