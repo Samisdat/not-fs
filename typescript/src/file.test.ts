a9a57cab4783994cd2c8180adf7bf67ae4f72cd9
Bastian Pertz <bastian.pertz@vodafone.com>
2017-06-28 21:27:18 +0200

    Migrate file

---
+++ typescript/src/file.test.ts
@@ -0,0 +1,78 @@
+'use strict';
+
+import { expect } from 'chai';
+
+import File from './file';
+
+describe('dir', function() {
+
+    let file:File;
+
+    beforeEach(function() {
+        file = new File(1, 'test');
+    });
+
+    it('can be created', function() {
+
+        expect(file).to.be.instanceof(File);
+
+    });
+
+    it('get it\'s inodenumber', function() {
+
+        expect(file.getInodeNumber()).to.be.equal(1);
+
+    });
+
+    it('get/set permission', function() {
+
+        expect(file.getPermission().getMode()).to.be.equal('0644');
+
+        file.setPermission('0777');
+        expect(file.getPermission().getMode()).to.be.equal('0777');
+
+    });
+
+    it('get/set it\'s name', function() {
+
+        expect(file.getName()).to.be.equal('test');
+
+        file.setName('foobar');
+        expect(file.getName()).to.be.equal('foobar');
+
+    });
+
+    it('isRoot is false', function() {
+
+        expect(file.isRoot()).to.be.false;
+
+    });
+
+    it('isFile is true', function() {
+
+        expect(file.isFile()).to.be.true;
+
+    });
+
+    it('isDirectory is false', function() {
+
+        expect(file.isDirectory()).to.be.false;
+
+    });
+
+    it('get/set content', function() {
+
+        expect(file.getContent()).to.be.equal('');
+
+        file.setContent('foobar');
+
+        expect(file.getContent()).to.be.equal('foobar');
+
+        file.setContent('');
+
+        expect(file.getContent()).to.be.equal('');
+
+    });
+
+});
+
---
+++ typescript/src/file.ts
@@ -0,0 +1,60 @@
+'use strict';
+
+import Node from './node';
+import Permission from './permission';
+
+export default class File extends Node {
+
+    private content:string;
+
+    constructor(inodeNumber:number, name:string, content = '') {
+
+        super(inodeNumber, name);
+
+        this.permission = new Permission('0644');
+
+        this.content = content;
+
+    }
+
+    public isRoot():boolean{
+
+        return false;
+
+    };
+
+    public isFile():boolean{
+
+        return true;
+
+    };
+
+    public isDirectory():boolean{
+
+        return false;
+
+    };
+
+    public getContent():string{
+
+        //this.getStats().atime = new Date();
+        return this.content;
+
+    };
+
+    public setContent(content:string):void{
+
+        if ( undefined === content){
+            content = '';
+        }
+
+        //this.getStats().atime = new Date();
+        //this.getStats().mtime = new Date();
+        //this.getStats().size = content.length;
+
+        this.content = content;
+
+    };
+    
+}
+
